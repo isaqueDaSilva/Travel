0f010d364ab1fc9893dda2d2d3bb4b39d5dcd2fb
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-12-10 07:32:00 -0300

    Creating a row to display ride informations

--- Travel/View/History/Components/RideHistoyRow+RideInformationContent.swift
+++ Travel/View/History/Components/RideHistoyRow+RideInformationContent.swift
@@ -1,12 +1,12 @@
 //
-//  RideInformationContent.swift
+//  RideHistoyRow+RideInformationContent.swift
 //  Travel
 //
 //  Created by Isaque da Silva on 12/10/24.
 //
 
-
 import SwiftUI
+
 extension RideHistoyRow {
     struct RideInformationContent: View {
         let title: String
--- Travel/View/History/Components/RideHistoyRow.swift
+++ Travel/View/History/Components/RideHistoyRow.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct RideHistoyRow: View {
+    let rideInformation: CustomerRideHistory.Ride
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox {
+            VStack {
+                RideInformationContent(
+                    title: "Data da Viagem",
+                    description: "\(rideInformation.date, format: .dateTime)"
+                )
+                
+                RideInformationContent(
+                    title: "Distância",
+                    description: "\(rideInformation.distance, format: .number) KM"
+                )
+                
+                RideInformationContent(
+                    title: "Duração",
+                    description: "\(rideInformation.duration) min."
+                )
+                
+                RideInformationContent(
+                    title: "Motorista",
+                    description: "\(rideInformation.driver.name)"
+                )
+                
+                RideInformationContent(
+                    title: "Valor",
+                    description: "\(rideInformation.value, format: .currency(code: "BRL"))"
+                )
+            }
+        } label: {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(rideInformation.origin)
+                    
+                    ForEach(1...3, id: \.self) { _ in
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 5, height: 5)
+                    }
+                    
+                    Text(rideInformation.destination)
+                }
+                
+                Rectangle()
+                    .frame(height: 1)
+            }
+        }
+
     }
 }
 
 #Preview {
-    RideHistoyRow()
+    RideHistoyRow(
+        rideInformation: .init(
+            id: 1,
+            date: .now,
+            origin: "Apple Park",
+            destination: "Apple Infinity Loop",
+            distance: 4.0,
+            duration: "7:00",
+            driver: .init(id: 1, name: "Tim Cook"),
+            value: 15.0
+        )
+    )
+    .padding()
 }
